f119a9e5decc2815c007e66bdd52e429c3500734
akshays244 <akshays244@gmail.com>
2024-04-10 09:59:00 +0000

    Initial commit

---
+++ provider.tf
@@ -0,0 +1,22 @@
+terraform {
+
+backend "gcs" {
+    bucket  = "roidtc-240408-user515-tf-state"
+    prefix  = "terraform/state"
+  }
+
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.2.0"
+    }
+  }
+}
+
+
+provider "google" {
+
+  project = var.project_id
+
+}
